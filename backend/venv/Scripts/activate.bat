47b0e17c7b7cfd0f84558d4d754d7cff19199ae3
ardaniah <ardaniahjamaluddin@gmail.com>
2023-09-10 00:45:41 +0800

    Initial commit

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Ardaniah Jamaluddin\Downloads\fastapi-reactjs-loginpage-main\backend\venv
+set VIRTUAL_ENV=C:\Users\Ardaniah Jamaluddin\Downloads\DAVINCI-CODE\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- backend/venv/Scripts/start.cmd
+++ backend/venv/Scripts/start.cmd
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\Ardaniah Jamaluddin\Downloads\fastapi-reactjs-loginpage-main\backend\venv\Scripts\python.exe" "%~dp0\start" %*
+"C:\Users\Ardaniah Jamaluddin\Downloads\DAVINCI-CODE\backend\venv\Scripts\python.exe" "%~dp0\start" %*
 
